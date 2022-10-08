b80bb9f5e8872408fc38d82eb76478c7c47f2e6b
Sergei Murashow <sergey.murashow@gmail.com>
2022-10-08 14:17:58 +1000

    add solution for 1-module 2-task

--- 02-event-loop/01-events-order/index.js
+++ 02-event-loop/01-events-order/index.js
@@ -1,23 +1,23 @@
 const intervalId = setInterval(() => {
-  console.log('James');
+  console.log('James'); // 2, 5
 }, 10);
 
 setTimeout(() => {
-  const promise = new Promise((resolve) => {
-    console.log('Richard');
-    resolve('Robert');
+  const promise = new Promise((resolve) => { 
+    console.log('Richard'); // 1
+    resolve('Robert'); // 3
   });
 
   promise
       .then((value) => {
-        console.log(value);
+        console.log(value); // 4
 
         setTimeout(() => {
-          console.log('Michael');
+          console.log('Michael'); // 6
 
           clearInterval(intervalId);
         }, 10);
       });
 
-  console.log('John');
+  console.log('John');// 2
 }, 10);
