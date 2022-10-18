c4bf5f8daf5f7bbbf57e9bf2319536b10babea6d
Sergei Murashow <sergey.murashow@gmail.com>
2022-10-18 23:58:32 +1000

    Lesson 3 task 1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,13 +2,22 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
+    constructor(options) {
+        super(options)
+        this.limit = options.limit
+        this.tmp = 0
+    }
 
-  _transform(chunk, encoding, callback) {
-
-  }
+    _transform(chunk, encoding, callback) {
+        let str = chunk.toString()
+        let bufferString = new Buffer.from(str)
+        this.tmp += bufferString.length
+        if (this.tmp > this.limit) {
+            callback(new LimitExceededError())
+            return
+        }
+        callback(null, chunk)
+    }
 }
 
 module.exports = LimitSizeStream;
--- 03-streams/01-limit-size-stream/index.js
+++ 03-streams/01-limit-size-stream/index.js
@@ -7,7 +7,7 @@ const outStream = fs.createWriteStream('out.txt');
 limitedStream.pipe(outStream);
 
 limitedStream.write('hello'); // 'hello' - это 5 байт, поэтому эта строчка целиком записана в файл
-
+limitedStream.write('😀');
 setTimeout(() => {
   limitedStream.write('world'); // ошибка LimitExceeded! в файле осталось только hello
 }, 10);
